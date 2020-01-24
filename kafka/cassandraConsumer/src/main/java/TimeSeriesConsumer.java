e9b2e2c3ee0195130a437cc16f76c419883780de
Johnboi <johndesmond631@gmail.com>
2020-01-24 15:46:28 -0500

    created jar to test consumer connection to cassandra

--- kafka/cassandraConsumer/src/main/java/TimeSeriesConsumer.java
+++ kafka/cassandraConsumer/src/main/java/TimeSeriesConsumer.java
@@ -1,29 +1,104 @@
-
-import com.google.gson.Gson;
+import com.datastax.driver.core.Metadata;
+import com.datastax.driver.core.Session;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.google.gson.JsonSyntaxException;
-import jdk.nashorn.internal.parser.JSONParser;
+
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
-
 import org.apache.kafka.common.serialization.StringDeserializer;
 
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
 import java.util.Arrays;
 import java.util.Properties;
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Cluster.Builder;
 
 
 public class TimeSeriesConsumer {
 
+
+
+    private Cluster cluster;
+    private Session session;
+    private static final Logger LOG = LoggerFactory.getLogger(TimeSeriesConsumer.class);
+
+
+    // creating table
+    private static final String TABLE_NAME = "simpleTimeSeries";
+
+    // connect to Cassandra
+    public void connect(String node, Integer port) {
+
+        Builder b = Cluster.builder().addContactPoint(node);
+
+        if (port != null) {
+            b.withPort(port);
+        }
+        cluster = b.build();
+
+        Metadata metadata = cluster.getMetadata();
+        LOG.info("Cluster name: " + metadata.getClusterName());
+
+        session = cluster.connect();
+
+    }
+
+    // get cassandra session
+    public Session getSession() {
+        return this.session;
+    }
+
+    // close cassandra connection
+    public void close() {
+        session.close();
+        cluster.close();
+    }
+
+    // Creating Cassandra KeySpace
+    public void createKeySpace(
+            String keyspaceName, String replicationStrategy, int replicationFactor) {
+        StringBuilder sb =
+                new StringBuilder("CREATE KEYSPACE IF NOT EXISTS")
+                .append(keyspaceName).append("WITH replication = {")
+                .append("'class':'").append(replicationStrategy)
+                .append("','replication_factor':").append(replicationFactor)
+                .append("};");
+
+    }
+
+    public void useKeyspace(String keyspace) {
+        session.execute("USE " + keyspace);
+    }
+
+    public void createTable() {
+        StringBuilder sb = new StringBuilder("CREATE TABLE IF NOT EXISTS ").append(TABLE_NAME).append("(")
+                .append("geohash uuid, timestampcol timestamp, energy double, PRIMARY KEY(geohash, timestampcol)) ")
+                .append("WITH CLUSTERING ORDER BY (timestampcol ASC);");
+
+        final String query = sb.toString();
+        session.execute(query);
+    }
+
     public static void main(String[] args) {
+        TimeSeriesConsumer tsc = new TimeSeriesConsumer();
+        // connect to cassandra
+        // I think I need to change the node to the public IP of
+        // public ip = ec2-3-231-170-82.compute-1.amazonaws.com
+        tsc.connect("10.0.0.5", 9042);
+
+        Session session = tsc.getSession();
+
+        tsc.createKeySpace("geoTime", "SimpleStrategy",
+                1);
+        tsc.useKeyspace("geoTime");
+
+
 
         // if you miss the tab for the class, you can get back to that
         // drop down menu with alt+Tab
@@ -60,9 +135,9 @@ public class TimeSeriesConsumer {
                 logger.info("Partition: " + record.partition() + ", Offset:" + record.offset());
 
                 String jsonStr = record.value();
-//                JSONParser parser = new JSONParser();
 
-                // https://www.baeldung.com/gson-string-to-jsonobject
+
+
                 try {
                     JsonObject jsonObject = new JsonParser().parse(jsonStr).getAsJsonObject();
                     String timestamp = jsonObject.get("date").getAsString();
@@ -74,13 +149,8 @@ public class TimeSeriesConsumer {
                 }
 
 
-
-
             }
         }
-
-
-
 
 
     }
