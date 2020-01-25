99bdbf29399a45f90afbfa2ddea43954211209a3
Johnboi <johndesmond631@gmail.com>
2020-01-24 20:50:20 -0500

    fix another small bug in cassandraConsumer

--- kafka/cassandraConsumer/src/main/java/TimeSeriesConsumer.java
+++ kafka/cassandraConsumer/src/main/java/TimeSeriesConsumer.java
@@ -64,8 +64,8 @@ public class TimeSeriesConsumer {
     public void createKeySpace(
             String keyspaceName, String replicationStrategy, int replicationFactor) {
         StringBuilder sb =
-                new StringBuilder("CREATE KEYSPACE IF NOT EXISTS")
-                .append(keyspaceName).append("WITH replication = {")
+                new StringBuilder("CREATE KEYSPACE IF NOT EXISTS ")
+                .append(keyspaceName).append(" WITH replication = {")
                 .append("'class':'").append(replicationStrategy)
                 .append("','replication_factor':").append(replicationFactor)
                 .append("};");
